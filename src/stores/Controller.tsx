c2f897e90aa346729fb2a0b9eff495eea5ea8010
devyubi <dev.munyubigmail.com>
2025-10-30 11:08:16 +0900

    [docs] zustand 미들웨어 기초

--- src/stores/Controller.tsx
+++ src/stores/Controller.tsx
@@ -1,18 +1,18 @@
 'use client';
 
 import { Button } from '@/components/ui/button';
-import { useCountStore, useIncrement } from './count';
+import { useDecrement, useIncrement } from './count';
 
 const Controller = () => {
   const increment = useIncrement();
-  const decrement = useIncrement();
-  const reset = useIncrement();
+  const decrement = useDecrement();
+  // const reset = useReset();
 
   return (
     <div className='flex gap-2'>
       <Button onClick={increment}>증가</Button>
       <Button onClick={decrement}>감소</Button>
-      <Button onClick={reset}>초기화</Button>
+      {/* <Button onClick={reset}>초기화</Button> */}
     </div>
   );
 };
